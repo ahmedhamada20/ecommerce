b282a8b3978e1e15023f16a41e620c7489041aea
ahmedhamada20 <hamada2031995@gmail.com>
2024-11-06 15:24:23 +0200

    edit

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -19,4 +19,8 @@ class Brand extends Model
         'description',
         'columns',
     ];
+
+   public function count() {
+        return $this->hasMany(Product::class,'brand_id')->count();
+    }
 }
