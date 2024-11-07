21816b383ba86362f8de190042b96cc7b16f4be8
ahmedhamada20 <hamada2031995@gmail.com>
2024-11-07 13:12:03 +0200

    edit

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -30,7 +30,7 @@ class ProductSeeder extends Seeder
         $sizes = QueryModelsAll('Size')->get();
 
         $products = [];
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i < 5; $i++) {
             $products[] = Product::create([
                 'name_ar' => fake()->name(),
                 'name_en' => fake()->name(),
