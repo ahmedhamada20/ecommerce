8f749c6f9c05b8d84481a3b4a031590c3085c5aa
ahmedhamada20 <hamada2031995@gmail.com>
2024-11-10 16:18:56 +0200

    edit

--- app/Http/Resources/Api/BlogResources.php
+++ app/Http/Resources/Api/BlogResources.php
@@ -17,7 +17,7 @@ class BlogResources extends JsonResource
         $lang = $request->header('language', 'en');
         return [
             'id' => $this->id,
-            'name' => $lang == "ar" ? $this->name_ar : $this->name_ar,
+            'name' => $lang == "ar" ? $this->name_ar : $this->name_en,
             'short_description' => $lang == "ar" ? $this->short_description_ar : $this->short_description_en,
             'description' => $lang == "ar" ? $this->description_ar : $this->description_en,
             'image' => asset('storage/blogs/'.$this->image),
