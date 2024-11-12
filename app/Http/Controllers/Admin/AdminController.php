96e6a9bd7e7786f28baddae6cb7ffb44dd5d5318
ahmedhamada20 <hamada2031995@gmail.com>
2024-11-12 11:40:33 +0200

    edit

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Info;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class AdminController extends Controller
 {
@@ -23,11 +24,59 @@ class AdminController extends Controller
     public function setting_update(Request $request)
     {
 
+        $logoName = null;
+        if ($request->hasFile('logo')) {
+            $logoName = time() . '_logo.' . $request->logo->extension();
+            $request->logo->move(storage_path('app/public/logo'), $logoName);
+        }
+
+
+        $paymentLogoName = null;
+        if ($request->hasFile('payment_logo')) {
+            $paymentLogoName = time() . '_payment_logo.' . $request->payment_logo->extension();
+            $request->payment_logo->move(storage_path('app/public/logo'), $paymentLogoName);
+        }
+
+
+        $homeOpenLogoName = null;
+        if ($request->hasFile('home_open_logo_new')) {
+            $homeOpenLogoName = time() . '_home_open_logo.' . $request->home_open_logo_new->extension();
+            $request->home_open_logo_new->move(storage_path('app/public/logo'), $homeOpenLogoName);
+        }
+
+
+        $partnersLogoName = null;
+        if ($request->hasFile('partners_logo')) {
+            $partnersLogoName = time() . '_partners_logo.' . $request->partners_logo->extension();
+            $request->partners_logo->move(storage_path('app/public/logo'), $partnersLogoName);
+        }
+
+        $categoryLogoName = null;
+        if ($request->hasFile('category_logo')) {
+            $categoryLogoName = time() . '_category_logo.' . $request->category_logo->extension();
+            $request->category_logo->move(storage_path('app/public/logo'), $categoryLogoName);
+        }
+
+
+        $banarLogoName = null;
+        if ($request->hasFile('banar_logo')) {
+            $banarLogoName = time() . '_banar_logo.' . $request->banar_logo->extension();
+            $request->banar_logo->move(storage_path('app/public/logo'), $banarLogoName);
+        }
+
+
+        $blogLogoName = null;
+        if ($request->hasFile('blog_logo')) {
+            $blogLogoName = time() . '_blog_logo.' . $request->blog_logo->extension();
+            $request->blog_logo->move(storage_path('app/public/logo'), $blogLogoName);
+        }
+
+
         Info::updateOrcreate([
             'id' => $request->id
         ], [
-            'name'=>$request->name,
-            'logo'=>$request->logo,
+            'name_ar'=>$request->name_ar,
+            'name_en'=>$request->name_en,
             'phone'=>$request->phone,
             'phone_1'=>$request->phone_1,
             'phone_2'=>$request->phone_2,
@@ -36,19 +85,27 @@ class AdminController extends Controller
             'fb_link'=>$request->fb_link,
             'tw_link'=>$request->tw_link,
             'in_link'=>$request->in_link,
-            'payment_logo'=>$request->payment_logo,
-            'home_open_logo_new'=>$request->home_open_logo_new,
-            'home_tilte_logo_new'=>$request->home_tilte_logo_new,
-            'home_title_products_1'=>$request->home_title_products_1,
-            'notes_title_products_1'=>$request->notes_title_products_1,
-            'home_title_products_2'=>$request->home_title_products_2,
-            'notes_title_products_2'=>$request->notes_title_products_2,
-            'partners_logo'=>$request->partners_logo,
-            'category_logo'=>$request->category_logo,
-            'banar_logo'=>$request->banar_logo,
-            'blog_logo'=>$request->blog_logo,
+            'home_tilte_logo_new_ar'=>$request->home_tilte_logo_new_ar,
+            'home_tilte_logo_new_en'=>$request->home_tilte_logo_new_en,
+            'home_title_products_1_ar'=>$request->home_title_products_1_ar,
+            'home_title_products_1_en'=>$request->home_title_products_1_en,
+            'notes_title_products_1_ar'=>$request->notes_title_products_1_ar,
+            'notes_title_products_1_en'=>$request->notes_title_products_1_en,
+            'home_title_products_2_ar'=>$request->home_title_products_2_ar,
+            'home_title_products_2_en'=>$request->home_title_products_2_en,
+            'notes_title_products_2_ar'=>$request->notes_title_products_2_ar,
+            'notes_title_products_2_en'=>$request->notes_title_products_2_en,
             'category_logo_title_ar'=>$request->category_logo_title_ar,
             'category_logo_title_en'=>$request->category_logo_title_en,
+
+
+            'logo' => $logoName ?? $request->logo,
+            'payment_logo' => $paymentLogoName ?? $request->payment_logo,
+            'home_open_logo_new' => $homeOpenLogoName ?? $request->home_open_logo_new,
+            'partners_logo' => $partnersLogoName ?? $request->partners_logo,
+            'category_logo' => $categoryLogoName ?? $request->category_logo,
+            'banar_logo' => $banarLogoName ?? $request->banar_logo,
+            'blog_logo' => $blogLogoName ?? $request->blog_logo,
         ]);
 
         return redirect()->back()->with([
--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -27,9 +27,56 @@
                            value="{{$data->name_en ?? null}}" required>
                 </div>
 
+            </div>
+
+            <br>
+
+            <div class="row">
+
                 <div class="col">
                     <label>رقم الهاتف</label>
                     <input type="number" class="form-control" name="phone" value="{{ $data->phone ?? '' }}" required>
+                </div>
+
+                <div class="col">
+                    <label>phone_1</label>
+                    <input type="number" class="form-control" name="phone_1" value="{{ $data->phone_1 ?? '' }}">
+                </div>
+
+                <div class="col">
+                    <label>phone_2</label>
+                    <input type="number" class="form-control" name="phone_2" value="{{ $data->phone_2 ?? '' }}">
+                </div>
+
+                <div class="col">
+                    <label>phone_3</label>
+                    <input type="number" class="form-control" name="phone_3" value="{{ $data->phone_3 ?? '' }}">
+                </div>
+
+                <div class="col">
+                    <label>phone_4</label>
+                    <input type="number" class="form-control" name="phone_4" value="{{ $data->phone_4 ?? '' }}">
+                </div>
+
+
+            </div>
+
+
+            <br>
+
+            <div class="row">
+                <div class="col">
+                    <label>facebook</label>
+                    <input type="url" class="form-control" name="fb_link" value="{{ $data->fb_link ?? '' }}">
+                </div>
+
+                <div class="col">
+                    <label>twitter</label>
+                    <input type="url" class="form-control" name="tw_link" value="{{ $data->tw_link ?? '' }}">
+                </div>
+                <div class="col">
+                    <label>linked</label>
+                    <input type="url" class="form-control" name="in_link" value="{{ $data->in_link ?? '' }}">
                 </div>
             </div>
 
@@ -37,20 +84,173 @@
 
             <div class="row">
                 <div class="col">
-                    <label>الوصف</label>
+                    <label>home_tilte_logo_new_ar</label>
+                    <input type="text" class="form-control" name="home_tilte_logo_new_ar"
+                           value="{{$data->home_tilte_logo_new_ar ?? null}}" required>
+                </div>
+                <div class="col">
+                    <label>home_tilte_logo_new_en</label>
+                    <input type="text" class="form-control" name="home_tilte_logo_new_en"
+                           value="{{$data->home_tilte_logo_new_en ?? null}}" required>
+                </div>
+                <div class="col">
+                    <label>home_title_products_1_ar</label>
+                    <input type="text" class="form-control" name="home_title_products_1_ar"
+                           value="{{$data->home_title_products_1_ar ?? null}}" required>
+                </div>
+                <div class="col">
+                    <label>home_title_products_1_en</label>
+                    <input type="text" class="form-control" name="home_title_products_1_en"
+                           value="{{$data->home_title_products_1_en ?? null}}" required>
+                </div>
+            </div>
 
-                    <textarea  type="text" class="form-control" style="height: 300px;"
-                              name="columns[notes]">
-                        {{ isset($data->columns) ? json_decode($data->columns)->notes : '' }}
+            <br>
+            <div class="row">
+                <div class="col">
+                    <label>notes_title_products_1_ar</label>
+                    <textarea class="form-control ckeditor" name="notes_title_products_1_ar" rows="2" required>
+                        {{$data->notes_title_products_1_ar ?? null}}
                     </textarea>
+                </div>
 
+                <div class="col">
+                    <label>notes_title_products_1_en</label>
+                    <textarea class="form-control ckeditor" name="notes_title_products_1_en" rows="2" required>
+                             {{$data->notes_title_products_1_ar ?? null}}
+                    </textarea>
+                </div>
+            </div>
+
+            <br>
+
+            <div class="row">
+                <div class="col">
+                    <label>home_title_products_2_ar</label>
+                    <input type="text" class="form-control" name="home_title_products_2_ar"
+                           value="{{$data->home_title_products_2_ar ?? null}}" required>
 
                 </div>
 
+                <div class="col">
+                    <label>home_title_products_2_en</label>
+                    <input type="text" class="form-control" name="home_title_products_2_en"
+                           value="{{$data->home_title_products_2_en ?? null}}" required>
+
+                </div>
+            </div>
+
+            <br>
+            <div class="row">
+                <div class="col">
+                    <label>notes_title_products_2_ar</label>
+                    <textarea class="form-control ckeditor" name="notes_title_products_2_ar" rows="2"
+                              required>     {{$data->notes_title_products_2_ar ?? null}}</textarea>
+                </div>
+
+                <div class="col">
+                    <label>notes_title_products_2_en</label>
+                    <textarea class="form-control ckeditor" name="notes_title_products_2_en" rows="2"
+                              required> {{$data->notes_title_products_2_en ?? null}}</textarea>
+                </div>
+            </div>
+            <br>
+
+
+            <div class="row">
+                <div class="col">
+                    <label>category_logo_title_ar</label>
+                    <input type="text" class="form-control" name="category_logo_title_ar"
+                           value="{{$data->category_logo_title_ar ?? null}}" required>
+                </div>
+
+                <div class="col">
+                    <label>category_logo_title_en</label>
+                    <input type="text" class="form-control" name="category_logo_title_en"
+                           value="{{$data->category_logo_title_en ?? null}}" required>
+                </div>
             </div>
 
 
             <br>
+
+            <div class="row">
+                <div class="col">
+                    <label>logo</label>
+                    <input type="file" name="logo" required class="form-control" accept="image/*">
+                    @if($data->logo)
+                        <img src="{{asset('storage/info/'.$data->logo)}}" width="100px"
+                             height="100px" alt="">
+                    @endif
+                </div>
+
+
+                <div class="col">
+                    <label>payment_logo</label>
+                    <input type="file" name="payment_logo" required class="form-control" accept="image/*">
+                    @if($data->payment_logo)
+                        <img src="{{asset('storage/info/'.$data->payment_logo)}}" width="100px"
+                             height="100px" alt="">
+                    @endif
+                </div>
+
+            </div>
+
+            <br>
+
+            <div class="row">
+                <div class="col">
+                    <label>home_open_logo_new</label>
+                    <input type="file" name="home_open_logo_new" required class="form-control" accept="image/*">
+                    @if($data->home_open_logo_new)
+                        <img src="{{asset('storage/info/'.$data->home_open_logo_new)}}" width="100px"
+                             height="100px" alt="">
+                    @endif
+                </div>
+
+                <div class="col">
+                    <label>partners_logo</label>
+                    <input type="file" name="partners_logo" required class="form-control" accept="image/*">
+                    @if($data->partners_logo)
+                        <img src="{{asset('storage/info/'.$data->partners_logo)}}" width="100px"
+                             height="100px" alt="">
+                    @endif
+                </div>
+            </div>
+
+            <br>
+            <div class="row">
+                <div class="col">
+                    <label>category_logo</label>
+                    <input type="file" name="category_logo" required class="form-control" accept="image/*">
+                    @if($data->category_logo)
+                        <img src="{{asset('storage/info/'.$data->category_logo)}}" width="100px"
+                             height="100px" alt="">
+                    @endif
+                </div>
+
+                <div class="col">
+                    <label>shop_logo</label>
+                    <input type="file" name="banar_logo" required class="form-control" accept="image/*">
+                    @if($data->banar_logo)
+                        <img src="{{asset('storage/info/'.$data->banar_logo)}}" width="100px"
+                             height="100px" alt="">
+                    @endif
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col">
+                    <label>blog_logo</label>
+                    <input type="file" name="blog_logo" required class="form-control" accept="image/*">
+                    @if($data->blog_logo)
+                        <img src="{{asset('storage/info/'.$data->blog_logo)}}" width="100px"
+                             height="100px" alt="">
+                    @endif
+                </div>
+            </div>
+            <br>
+
 
             <div class="row">
                 <div class="col">
