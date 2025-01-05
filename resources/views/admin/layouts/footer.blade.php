2c3840a7d62d185f1d5a6d0519ec516d454a6ebb
ahmedhamada20 <hamada2031995@gmail.com>
2025-01-05 16:53:09 +0200

    edit

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,11 +1,20 @@
-<!-- ..::  footer  start ::.. -->
+<!-- ..:: Footer Start ::.. -->
 <footer class="d-footer">
     <div class="row align-items-center justify-content-between">
+        <!-- Left Section -->
         <div class="col-auto">
-            <p class="mb-0">© {{date('Y')}} - {{date('Y') + 1 }} WowDash. All Rights Reserved.</p>
+            <p class="mb-0">
+                © {{ date('Y') }} - {{ date('Y') + 1 }}
+                <a href="https://faroukgroup.com/ar" target="_blank" class="text-primary-600">Farouk Group</a>.
+            </p>
         </div>
+
+        <!-- Right Section -->
         <div class="col-auto">
-            <p class="mb-0">Made by <span class="text-primary-600">wowtheme7</span></p>
+            <p class="mb-0">
+                Made by <span class="text-primary-600">Farouk Group</span>
+            </p>
         </div>
     </div>
-</footer> <!-- ..::  footer area end ::.. -->
+</footer>
+<!-- ..:: Footer End ::.. -->
